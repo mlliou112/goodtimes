1091fd333f57a0441775d75c47d8c670f5c77c20
Abhishek Upadhyaya Ghimire <au15@duke.edu>
2015-10-10 18:54:21 -0400

    GUI and intro screens

--- goodtimes/ViewController.swift
+++ goodtimes/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    // MARK: Properties
+    let passcode = "1234"
+    
+    
+    @IBOutlet weak var passcodeTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
     }
-
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        passcodeTextField.text = textField.text
+    }
+    
+    
+    // MARK: Actions
+    
+    @IBAction func submitButton(sender: UIButton) {
+        //checkCode.text = "Default Text"
+        if passcodeTextField.text == passcode{
+            print("Button pressed 👍")
+        } else {
+            let alertController = UIAlertController(title: "Incorrect Passcode", message:
+                "Enter correct passcode!", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
+    
 
 }
 
