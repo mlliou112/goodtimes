17664a1ff66c548538a731e2a3171d2fda946d26
Abhishek Upadhyaya Ghimire <au15@duke.edu>
2015-10-10 21:25:24 -0400

    everything merged and working

--- goodtimes/ViewController.swift
+++ goodtimes/ViewController.swift
@@ -3,23 +3,44 @@
 //  goodtimes
 //
 //  Created by Michael Liou on 10/10/15.
-//  Copyright © 2015 Michael Liou. All rights reserved.
+//  Copyright © 2015 Abhishek Upadhyaya Ghimire. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
+    let passcode = "1234"
+    
+    @IBOutlet weak var passcodeTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
     }
-
-
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        passcodeTextField.text = textField.text
+    }
+    // MARK: Actions
+    
+    @IBAction func submitButton(sender: UIButton) {
+        //print("Button pressed 👍")
+        if passcodeTextField.text == passcode{
+            self.performSegueWithIdentifier("segueIdentifier", sender: self)
+        }else{
+            let alertController = UIAlertController(title: "Incorrect Passcode", message:
+                "Enter correct passcode!", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
+    
 }
-
